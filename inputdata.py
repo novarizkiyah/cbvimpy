983f9c81bdcbe8e0109cc1ea1bbd320b966f84d8
novarizkiyah <novalailatulrizkiyah@gmail.com>
2022-11-15 14:06:57 +0700

    15 nov

---
+++ inputdata.py
@@ -0,0 +1,9 @@
+
+data = input("Masukkan data:")
+print("data: ", data, type(data))
+
+data2 = int(input("Masukkan data:"))
+print("data: ", data2, type(data2))
+
+biner= bool(int(input("Masukkan data:")))
+print("data: ", biner, type(biner))

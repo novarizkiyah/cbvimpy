893787831d419d409ceaaf641cf28ce7a1e178c9
novarizkiyah <novalailatulrizkiyah@gmail.com>
2022-11-18 11:07:15 +0700

    18 nov 11.07

---
+++ module.py
@@ -0,0 +1,10 @@
+import matematika
+
+hasil_tambah = matematika.tambah(1,2,3,4,5)
+print(f"hasil tambah = {hasil_tambah}")
+
+hasil_kali = matematika.kali(1,2,3,4,5)
+print(f"hasil kali = {hasil_kali}")
+
+pangkat_dua = matematika.pangkat(2)
+print(f"hasil pangkat dua= {pangkat_dua(2)}")

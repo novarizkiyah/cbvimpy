4de4f21bf7cd0f2bbae096f72719690454a43661
novarizkiyah <novalailatulrizkiyah@gmail.com>
2022-11-18 17:17:41 +0700

    18 nov 17.17

---
+++ main_alias.py
@@ -0,0 +1,12 @@
+from matematika import tambah as add
+from matematika import kali as k
+from matematika import pangkat as p
+
+hasil_tambah = add(1,23,23,45)
+print(f"hasil tambah = {hasil_tambah}")
+
+hasil_kali = k(1,23,23,45)
+print(f"hasil kali = {hasil_kali}")
+
+pangkat_dua = p(2)
+print(f"hasil pangkat dua= {pangkat_dua(2)}")
---
+++ main_from.py
@@ -0,0 +1,13 @@
+
+
+from matematika import tambah, kali, pangkat
+#from matematika import *
+
+hasil_tambah = tambah(1,23,23,45)
+print(f"hasil tambah = {hasil_tambah}")
+
+hasil_kali = kali(1,23,23,45)
+print(f"hasil kali = {hasil_kali}")
+
+pangkat_dua = pangkat(2)
+print(f"hasil pangkat dua= {pangkat_dua(2)}")

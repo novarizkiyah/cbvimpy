7b7664cf87424eb8d86039b46172a0ed0c81faa6
novarizkiyah <novalailatulrizkiyah@gmail.com>
2022-11-23 11:08:03 +0700

    23 nov 11.07

---
+++ modul 57/main_app.py
@@ -0,0 +1,23 @@
+import sains 
+
+
+hasil_tambah=sains.matematika.tambah(2,3,1,34)
+hasil_kali=sains.matematika.kali(8,2,1,3)
+hasil_pangkat=sains.matematika.pangkat(3)
+hasul_fisika=sains.pisika.gaya(8,9)
+
+print(hasil_tambah)
+print(hasul_fisika)
+print(hasil_pangkat(2))
+print(hasil_kali)
+
+# from sains import *
+# hasil_tambah=matematika.basic.tambah(2,3,1,34)
+# hasil_kali=matematika.basic.kali(8,2,1,3)
+# hasil_pangkat=matematika.scientific.pangkat(3)
+# hasul_fisika=pisika.gaya(8,9)
+
+# print(hasil_tambah)
+# print(hasul_fisika)
+# print(hasil_pangkat(2))
+# print(hasil_kali)

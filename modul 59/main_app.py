5ef2c8186c54e8a852ce50dcb3fa536f45a56e89
novarizkiyah <novalailatulrizkiyah@gmail.com>
2022-11-29 10:23:46 +0700

    29 nov wk

--- modul 59/main_app.py
+++ modul 59/main_app.py
@@ -8,6 +8,8 @@ window.configure(bg="magenta")
 window.geometry("300x200")
 window.resizable(False, False)
 window.title("Notes Apps")
+NAMA_DEPAN = tk.StringVar()
+NAMA_BELAKANG = tk.StringVar()
 
 #frame input
 input_frame=ttk.Frame(window)
@@ -15,12 +17,24 @@ input_frame=ttk.Frame(window)
 input_frame.pack(padx=10, pady=10, fill="x", expand=True)
 
 #komponen2
-# 1/ label nama depan
+# 1. label nama depan
 nama_depan_label = ttk.Label(input_frame,text="Nama Depan")
 nama_depan_label.pack(fill="x", expand=True)
-NAMA_DEPAN = tk.StringVar()
+# 2. entry nama depan
+
 nama_depan_entry=ttk.Entry(input_frame,textvariable=NAMA_DEPAN)
 nama_depan_entry.pack(fill="x", expand=True)
+# 3. Label nama belakang
+nama_belakang_label = ttk.Label(input_frame,text="Nama Belakang")
+nama_belakang_label.pack(fill="x", expand=True)
+#4. entry nama belakang
+
+nama_belakang_entry=ttk.Entry(input_frame,textvariable=NAMA_BELAKANG)
+nama_belakang_entry.pack(fill="x", expand=True)
+
+# 5. Tombol
+#tombol_sapa.ttk.Button(input_frame)
+#oke ini sih wkwkwkwkw
 
 
 window.mainloop()

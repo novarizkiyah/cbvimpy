fdd816415f1296b6791fa70a523f53a78f2990ef
novarizkiyah <novalailatulrizkiyah@gmail.com>
2022-12-25 19:15:31 +0700

    25 des

--- modul 59/main_app.py
+++ modul 59/main_app.py
@@ -2,14 +2,23 @@
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter.messagebox import showinfo
 
+#Init
 window = tk.Tk()
 window.configure(bg="magenta")
 window.geometry("300x200")
 window.resizable(False, False)
 window.title("Notes Apps")
+
+#Variabel dan fungsi
 NAMA_DEPAN = tk.StringVar()
 NAMA_BELAKANG = tk.StringVar()
+def tombol_click():
+    print(NAMA_BELAKANG.get())
+    '''fungsi ini akan dipanggil tombol'''
+    pesan = f"Halo, {NAMA_DEPAN.get()} {NAMA_BELAKANG.get()}, Selamat Datang!"
+    showinfo(title="Hai", message=pesan)
 
 #frame input
 input_frame=ttk.Frame(window)
@@ -33,8 +42,12 @@ nama_belakang_entry=ttk.Entry(input_frame,textvariable=NAMA_BELAKANG)
 nama_belakang_entry.pack(fill="x", expand=True)
 
 # 5. Tombol
-#tombol_sapa.ttk.Button(input_frame)
-#oke ini sih wkwkwkwkw
+
+
+tombol_sapa = ttk.Button(input_frame,text="Sapa", command=tombol_click)
+tombol_sapa.pack(fill='x', expand=True, padx=10, pady=10)
+
+
 
 
 window.mainloop()

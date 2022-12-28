fb658107ae626d1e76c13b261baeda864763a80b
novarizkiyah <novalailatulrizkiyah@gmail.com>
2022-12-28 20:03:11 +0700

    28 des 20.03

---
+++ modul 64/main_app.py
@@ -0,0 +1,15 @@
+## tutorial membaca eksternal
+
+# import os
+
+
+# path = "/home/nova/Python-3.9.1/project-1/modul_64"
+# os.chdir(path)
+
+
+# file = open("~/data.txt",mode="r")
+# print(file)
+
+print(3*"=", "Membaca file txt dengan bhaique", 3*"=")
+f = open("data.txt", mode="r")
+print(f.read())

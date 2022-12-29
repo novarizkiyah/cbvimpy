a8062a92ac4b85428f4b26c62a6bbe6236c4bd20
novarizkiyah <novalailatulrizkiyah@gmail.com>
2022-12-29 13:30:22 +0700

    29 Des 2022 13.30

--- modul 64/main_app.py
+++ modul 64/main_app.py
@@ -1,15 +1,30 @@
 ## tutorial membaca eksternal
 
-# import os
-
-
-# path = "/home/nova/Python-3.9.1/project-1/modul_64"
-# os.chdir(path)
-
-
-# file = open("~/data.txt",mode="r")
-# print(file)
-
 print(3*"=", "Membaca file txt dengan bhaique", 3*"=")
-f = open("data.txt", mode="r")
-print(f.read())
+file= open("data.txt", mode="r")
+
+print(f"Status readable = {file.readable()}")
+print(f"Status writable = {file.writable()}")
+
+#baca seluruh file
+print(file.read())
+
+# print(file.readline(),end="") #baca baris pertama
+# print(file.readline(),end="")
+
+#baca semua baris sebagai list
+#print(file.readlines())
+
+print(f"Apalah file sudah closed = {file.closed}")
+file.close()
+print(f"Apalah file sudah closed = {file.closed}")
+
+##salah satu cara untuk membuka file di python
+
+print(3*"=", "Membaca file txt dengan with", 3*"=")
+with open("data.txt", mode="r") as file:
+    content = file.read()
+    print(content, end="\n")
+    print(f"Apakah sudah diclose ={file.closed}")
+
+print(f"Apakah sudah diclose ={file.closed}")

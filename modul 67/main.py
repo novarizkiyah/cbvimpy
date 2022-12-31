910eac313ea8a75e547fec31acb0cbdfb62b0056
novarizkiyah <novalailatulrizkiyah@gmail.com>
2022-12-31 16:10:08 +0700

    31 des 16.10

--- modul 67/main.py
+++ modul 67/main.py
@@ -6,11 +6,22 @@ import os
 if __name__ == "__main__":
     sistem_operasi = os.name
 
-    match sistem_operasi:
-        case "posix": os.system("clear")
-        case "nt": os.system("cls")
-    print("Selamat Datang di Program")
-    print("DATABASE PERPUSTAKAAN")
-    print("========================")
+    
+
+    while(True):
+        match sistem_operasi:
+            case "posix": os.system("clear")
+            case "nt": os.system("cls")
+
+        print("Selamat Datang di Program")
+        print("DATABASE PERPUSTAKAAN")
+        print("========================")
+
+        print(f"1. Read Data")
+        print(f"2. Creat Data")
+        print(f"3. Update Data")
+        print(f"4. Delete Data")
+
+        user_option = input("Masukkan opsi: ")
     
 
